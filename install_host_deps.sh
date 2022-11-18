e5a906a9fc4040fa2e27377dd9c5c8b1340ec05e
nix <ngarafol@gmail.com>
2022-11-18 10:58:02 +0100

    add master name and lock ansible to 6.5.0 because of lxc_container issue

--- install_host_deps.sh
+++ install_host_deps.sh
@@ -4,7 +4,7 @@ set -x
 sudo apt update
 sudo apt install python3 python3-pip python3-lxc libssl-dev git -y
 pip3 install --upgrade setuptools
-pip3 install ansible
+pip3 install ansible==6.5.0
 pip3 install -U cryptography
 
 echo 'installed ansible to ~/.local/bin, make sure this is in $PATH'
